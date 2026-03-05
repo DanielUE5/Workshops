5cef67592f7dd0d422a5047c8a65c02840df4832
Daniel <ivanovdaniel51@gmail.com>
2026-03-05 21:40:41 +0200

    Add BaseController with GetUserId; update HomeController
    Introduced BaseController to centralize user helper methods, including GetUserId(). Updated HomeController to inherit from BaseController and added [AllowAnonymous] for unauthenticated access. This improves code reuse and maintainability.

---
+++ CinemaApp/CinemaWeb/CinemaApp/Controllers/BaseController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace CinemaApp.Web.Controllers
+{
+    public class BaseController : Controller
+    {
+        public string? GetUserId()
+        {
+            return User.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+    }
+}
