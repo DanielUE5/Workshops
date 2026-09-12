211321ef448dd4451f4a519393e5c9eded370667
Daniel <ivanovdaniel51@gmail.com>
2026-09-12 13:27:45 +0300

    feat: add Point, Wall and Food base classes
    - Add Point coordinates and drawing methods
    - Implement Wall borders and initialization
    - Add abstract Food class with random positioning
    - Add food collision detection

---
+++ The game - Snake/The snake/SimpleSnake/GameObjects/Point.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SimpleSnake.GameObjects
+{
+    public class Point
+    {
+        public int LeftX { get; set; }
+        public int TopY { get; set; }
+
+        public Point(int leftX, int topY)
+        {
+            this.LeftX = leftX;
+            this.TopY = topY;
+        }
+
+        public void Draw(char symbol)
+        {
+            Console.SetCursorPosition(LeftX, TopY);
+            Console.Write(symbol);
+        }
+
+        public void Draw(int leftX, int topY, char symbol)
+        {
+            Console.SetCursorPosition(leftX, topY);
+            Console.Write(symbol);
+        }
+    }
+}
---
+++ The game - Snake/The snake/SimpleSnake/GameObjects/Wall.cs
@@ -0,0 +1,38 @@
+﻿namespace SimpleSnake.GameObjects
+{
+    public class Wall : Point
+    {
+        private const char wallSymbol = '\u25A0';
+
+        public Wall(int leftX, int topY)
+            : base(leftX, topY)
+        {
+            this.InitializeWallBordes();
+        }
+
+        private void SetHorizontalLine(int topY)
+        {
+            for (int leftX = 0; leftX < this.LeftX; leftX++)
+            {
+                this.Draw(leftX, topY, wallSymbol);
+            }
+        }
+
+        private void SetVerticalLine(int leftX)
+        {
+            for (int topY = 0; topY < this.TopY; topY++)
+            {
+                this.Draw(leftX, topY, wallSymbol);
+            }
+        }
+
+        private void InitializeWallBordes()
+        {
+            SetHorizontalLine(0);
+            SetHorizontalLine(this.TopY - 1);
+
+            SetVerticalLine(0);
+            SetVerticalLine(this.LeftX - 1);
+        }
+    }
+}
